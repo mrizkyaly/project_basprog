37fb5a9836c70f7ecd1c8eab23503cac18bebd5c
mrizkyaly <44080282+mrizkyaly@users.noreply.github.com>
2021-02-03 13:53:57 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,25 @@
 $(document).ready(function () {
+    // Scroll
     $(window).on('scroll', function () {
         if ($(window).scrollTop() > 10) {
             $('.navbar').addClass('active p-3');
         } else {
             $('.navbar').removeClass('active p-3');
         }
+    });
+
+    // Filter News
+    let $btns = $('.card-filter .nav-item .nav-link');
+
+    $btns.click(function (e) {
+        $('.card-filter .nav-item .nav-link').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('#news .list').isotope({
+            filter: selector
+        });
+
+        return false;
     });
 });
